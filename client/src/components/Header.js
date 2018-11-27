75f03fe1d94d6ea6a9f271d2d69a01ca11e613e1
Werto300 <werto300@gmail.com>
2018-11-27 06:55:13 -0600

    Primer commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component{
+
+    renderContent(){
+        console.log("Estado "+this.props.auth);
+        switch(this.props.auth){
+            case null:
+                return ;
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>
+        }
+    }
+
+    render(){
+        return(
+            <nav>
+                <div className="nav-wrapper">
+                <a className="left brand-logo">
+                    Emaily
+                </a>
+                <ul className="right">
+                    <li>
+                        { this.renderContent() }
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps({ auth }){
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
